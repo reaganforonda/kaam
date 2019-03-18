6bd2ed9cc14a52f6d4841a78f742d62164d4edda
reaganforonda <foronda.reagan@gmail.com>
2019-03-17 22:01:41 -0600

    landing

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+        font-size: 3em;
+        text-transform: uppercase;
+        text-align: center;
+        color: white;
+    `
+
+const Header = styled.header`
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+
+`
+const Main = styled.div`
+    background: black;
+    height: 100vh;
+`
+
 
 
 export default class Landing extends React.Component{
     render(){
         return (
-            <div className="landing">
-                Landing Page
-            </div>
+            <Main>
+                <Header/>
+                    <Title>Kaam</Title>
+                <Header/>
+                <div>
+                    Hello World
+                </div>
+            </Main>
         )
     }
 }
