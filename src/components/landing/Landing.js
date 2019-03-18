d46eab1de172a692199f0edb2f09a55e8ff80885
reaganforonda <foronda.reagan@gmail.com>
2019-03-17 21:05:35 -0600

    landing

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class Landing extends React.Component{
+    render(){
+        return (
+            <div className="landing">
+                Landing Page
+            </div>
+        )
+    }
+}
