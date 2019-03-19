693edaa87ce4a18fc10325012a34c71ae1598f67
reaganforonda <foronda.reagan@gmail.com>
2019-03-18 19:26:33 -0600

    landing

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -9,7 +9,7 @@ const Title = styled.h1`
     `
 
 const Header = styled.header`
-    border: 1px solid black;
+    border: 1px solid white;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -24,19 +24,46 @@ const Main = styled.div`
 const LoginSection = styled.div`
     border: solid white 2px;
     display: flex;
+    flex-direction: column;
 `
 
-const Input = styled.input`
+const Input = styled.input`b
+    width: 100px;
+`
+
+const HeaderNav = styled.nav`
+    display: flex;
+    padding: 40px 4px;
+`
+
+const HeaderMenu = styled.div`
 
 `
 
+const Button = styled.button`
+    background: ${props => props.primary ? "yellow" : "blue"}
+    color: ${props => props.primary ? "white" : "blue"}
+    font-size: 1em;
+    margin: 0.25em 1em;
+    border: 2px solid yellow;
+    border-radius: 3px;
+    
+    &:hover {
+        color: pink;
+    }
+`
 
 export default class Landing extends React.Component{
     render(){
         return (
             <Main>
                 <Header/>
-                    <Title>Kaam</Title>
+                    <HeaderNav>
+                        <Title>Kaam</Title>
+                        <HeaderMenu>
+                            <Button primary type='button'>SIGN UP</Button>
+                        </HeaderMenu>
+                    </HeaderNav>
                 <Header/>
                 <LoginSection>
                     <Input placeholder='Email' type='email'/>
