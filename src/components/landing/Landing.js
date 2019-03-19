678390d89e7956ceadaa4d0dfa6bb4509219f449
reaganforonda <foronda.reagan@gmail.com>
2019-03-18 20:20:59 -0600

    handleinputchange

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,76 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Title = styled.h1`
-        font-size: 3em;
-        text-transform: uppercase;
-        text-align: center;
-        color: white;
-    `
 
-const Header = styled.header`
-    border: 1px solid white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100px;
-
-`
 const Main = styled.div`
-    background: black;
+
     height: 100vh;
 `
 
-const LoginSection = styled.div`
-    border: solid white 2px;
-    display: flex;
-    flex-direction: column;
-`
-
-const Input = styled.input`b
-    width: 100px;
-`
-
-const HeaderNav = styled.nav`
-    display: flex;
-    padding: 40px 4px;
-`
-
-const HeaderMenu = styled.div`
-
-`
-
-const Button = styled.button`
-    background: ${props => props.primary ? "yellow" : "blue"}
-    color: ${props => props.primary ? "white" : "blue"}
-    font-size: 1em;
-    margin: 0.25em 1em;
-    border: 2px solid yellow;
-    border-radius: 3px;
-    
-    &:hover {
-        color: pink;
-    }
-`
 
 export default class Landing extends React.Component{
+    constructor(props ){
+        super(props);
+
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPW: '',
+            displayLogin: false
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }    
+
+    handleInputChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render(){
         return (
             <Main>
-                <Header/>
-                    <HeaderNav>
-                        <Title>Kaam</Title>
-                        <HeaderMenu>
-                            <Button primary type='button'>SIGN UP</Button>
-                        </HeaderMenu>
-                    </HeaderNav>
-                <Header/>
-                <LoginSection>
-                    <Input placeholder='Email' type='email'/>
-                    <Input placeholder='Company' type='text'/>
-                    <Input placeholder='First Name' type='text'/>
-                    <Input placeholder='Last Name' type='text'/>
-                </LoginSection>
+                <div>
+                    <header>
+                        <h1>Kaam</h1>
+                    </header>
+                </div>
+                <div>
+                    <div>
+                        <div></div>
+                        <div>
+                            <p>Sign Up</p>
+                            <p>or Sine In to your account</p>
+                        </div>
+                        <div>
+                            <input name='firstName' placeHolder='First Name' type='text' onChange={(e)=> this.handleInputChange(e)}/>
+                            <input name='lastName' placeHolder='Last Name' type='text' onChange={(e)=> this.handleInputChange(e)}/>
+                            <input name='email' placeholder='Email' type='password' onChange={(e)=> this.handleInputChange(e)} />
+                            <input name='password' placeholder='Password'/>
+                            <input name='password' placeholder='Confirm Password'/>
+                            <button>Sign Up</button>
+                        </div>
+                    </div>
+                </div>
             </Main>
         )
     }
