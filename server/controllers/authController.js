37a9019491771c446c96cedc21a789ca7f9476f9
reaganforonda <foronda.reagan@gmail.com>
2019-03-19 20:36:00 -0600

    added register to authcontroller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,28 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-    
+    register : async (req, res) => {
+        const db = req.app.get('db');
+        const {email, password, confirmPW, firstName, lastName} = req.body;
+
+        if(password !== confirmPW) {
+            res.sendStatus (400);
+        };
+
+        db.CHECK_EMAIL([email.toLowerCase()]).then((users) => {
+            if(users.length !== 0) {
+                if(users[0].email === email.toLowerCase()) {
+                    res.sendStatus(400);
+                } else {
+                    res.sendStatus(401);
+                }
+            } else {
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(confirmPW, salt);
+
+
+                // CREATE USER HERE
+            }
+        })
+    }
 }
