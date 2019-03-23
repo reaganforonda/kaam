225717366bdcc205a2bf4bb76c0f38aaf6a5b6cf
reaganforonda <foronda.reagan@gmail.com>
2019-03-22 21:59:18 -0500

    sign up form

--- src/components/landing/SignUpForm.js
+++ src/components/landing/SignUpForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class SignUpForm extends React.Component{
     constructor(props) {
@@ -22,22 +23,38 @@ export default class SignUpForm extends React.Component{
 
     handleFormSubmit(e) {
         e.preventDefault();
+        let user = Object.assign({}, this.state);
+        if(user.confirmPW === user.password) {
+            if(user.confirmPW !== '' && user.password !== '') {
+                axios.post('/api/auth/register', user).then((result) => {
+                    console.log(result);
+                })
+            }
+        }
+    }
 
-        console.log(this.state);
+    resetState(){
+        this.setState({
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPW: ''
+        })
     }
 
     render(){
         return (
             <form onSubmit={(e)=>this.handleFormSubmit(e)}>
-                <input name='firstName' type='text' placeholder='First Name' 
+                <input required name='firstName' type='text' placeholder='First Name' 
                     onChange={(e)=>this.handleInputChange(e)}/>
-                <input name='lastName' type='text' placeholder='Last Name' 
+                <input required name='lastName' type='text' placeholder='Last Name' 
                     onChange={(e)=>this.handleInputChange(e)}/>
-                <input name='email' type='email' placeholder='Email' 
+                <input required name='email' type='email' placeholder='Email' 
                     onChange={(e)=>this.handleInputChange(e)}/>
-                <input name='password' type='password' placeholder='Password' 
+                <input required name='password' type='password' placeholder='Password' 
                     onChange={(e)=>this.handleInputChange(e)}/>
-                <input name='confirmPW' type='password' placeholder='Confirm Password' 
+                <input required name='confirmPW' type='password' placeholder='Confirm Password' 
                     onChange={(e)=>this.handleInputChange(e)}/>
                 <button onClick={(e)=>this.handleFormSubmit(e)}>Submit</button>
             </form>
