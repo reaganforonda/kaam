a9563f4ef8c5b3b3d7b5f1531a4a803009e9c6d1
reaganforonda <foronda.reagan@gmail.com>
2019-04-10 22:14:04 -0600

    create add property form

---
+++ src/components/property/AddPropertyForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class AddPropertyForm extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={
+            propertyName:'',
+            acctSysCode: '',
+            address:''
+        }
+    }
+
+    render(){
+        return (
+            <form>
+                
+            </form>
+        )
+    }
+}
