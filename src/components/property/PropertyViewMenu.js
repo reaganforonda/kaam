b01073f533208b38096d2d8c3973e35fba9c5a5d
reaganforonda <foronda.reagan@gmail.com>
2019-04-10 22:07:47 -0600

    added prop menu

---
+++ src/components/property/PropertyViewMenu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class PropertyViewMenu extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={}
+    }
+
+    render(){
+        return (
+            <div>
+                <ul>
+                    <li>Add New Property</li>
+                    <li></li>
+                </ul>
+            </div>
+        )
+    }
+}
