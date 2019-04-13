41c1ae8b8ae48e6505e8f46d607ef59f89462a17
reaganforonda <foronda.reagan@gmail.com>
2019-04-13 10:43:45 -0600

    properyt list

---
+++ src/components/property/PropertyList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default class PropertyList extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state={}
+    }
+
+    render(){
+        return (
+            <div>Property List</div>
+        )
+    }
+}
