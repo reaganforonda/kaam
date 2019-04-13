a92f45ad2e11ee187c7f89384f73387207988d08
reaganforonda <foronda.reagan@gmail.com>
2019-04-13 10:34:47 -0600

    added submit form

--- src/components/property/AddPropertyForm.js
+++ src/components/property/AddPropertyForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class AddPropertyForm extends React.Component{
     constructor(props) {
@@ -7,11 +8,11 @@ export default class AddPropertyForm extends React.Component{
         this.state={
             propertyName:'',
             acctSysCode: '',
-            address:''
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.handleResetForm = this.handleResetForm.bind(this);
     }
 
     handleInputChange(e) {
@@ -20,15 +21,36 @@ export default class AddPropertyForm extends React.Component{
 
     handleFormSubmit(e) {
         e.preventDefault();
-        console.log(this.state);
+        axios.post('/api/property', this.state).then(()=> {
+            // TODO:
+            console.log('Added OK');
+            this.handleResetForm();
+        }).catch((err)=> {
+            // TODO:
+            console.log(`Error: ${err}`);
+        })
+    }
+
+    handleResetForm(){
+        this.setState({
+            propertyName:'',
+            acctSysCode: '',
+        })
     }
 
     render(){
         return (
             <form onSubmit={(e)=>this.handleFormSubmit(e)}>
                 <div>
-                    <input name='propertyName' placeholder='Property Name' type='text' onChange={(e)=>this.handleInputChange(e)}/>
-                    <input name='acctSysCode' placeholder='Accounting System ID' type='text' onChange={(e)=>this.handleInputChange(e)}/>
+                    <input name='propertyName' placeholder='Property Name' type='text' onChange={(e)=>this.handleInputChange(e)} 
+                        value={this.state.propertyName}/>
+                </div>
+                <div>
+                    <input name='acctSysCode' placeholder='Accounting System ID' type='text' onChange={(e)=>this.handleInputChange(e)} 
+                        value={this.state.acctSysCode}/>
+                </div>
+                <div>
+                    <button onClick={(e)=>this.handleFormSubmit(e)}>Submit</button>
                 </div>
             </form>
         )
