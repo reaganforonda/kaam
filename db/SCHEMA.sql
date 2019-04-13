d1fe7d6456aceded8c6c0f23cb47034b0a08fb88
reaganforonda <foronda.reagan@gmail.com>
2019-04-13 09:22:31 -0600

    add create table for property

--- db/SCHEMA.sql
+++ db/SCHEMA.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS properties;
 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
@@ -7,3 +8,10 @@ CREATE TABLE users (
     first_name VARCHAR(45),
     last_name VARCHAR(45)
 );
+
+
+CREATE TABLE properties(
+    property_id SERIAL PRIMARY KEY,
+    acct_sys_id VARCHAR(10),
+    property_name VARCHAR(45)
+)
