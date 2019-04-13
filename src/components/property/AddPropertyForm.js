e1ca9a552e5609bbefea53d2f2167751aab48602
reaganforonda <foronda.reagan@gmail.com>
2019-04-13 09:10:11 -0600

    add inputs to form

--- src/components/property/AddPropertyForm.js
+++ src/components/property/AddPropertyForm.js
@@ -9,13 +9,25 @@ export default class AddPropertyForm extends React.Component{
             acctSysCode: '',
             address:''
         }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    handleFormSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
     }
 
     render(){
         return (
-            <form>
+            <form onSubmit={(e)=>this.handleFormSubmit(e)}>
                 <div>
-                    <input name='propertyName' />
+                    <input name='propertyName' placeholder='Property Name' type='text' onChange={(e)=>this.handleInputChange(e)}/>
                 </div>
             </form>
         )
