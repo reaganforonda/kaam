026f1e0ea38bf67555be841d4efe7f30cc7f92a4
reaganforonda <foronda.reagan@gmail.com>
2019-04-13 09:55:40 -0600

    add property

--- server/controllers/propertyController.js
+++ server/controllers/propertyController.js
@@ -5,7 +5,12 @@ module.exports = {
         const db = req.app.get('db');
         const {propertyName, acctSysCode} = req.body;
 
-        
+        db.ADD_PROPERTY([propertyName, acctSysCode]).then(()=> {
+            res.sendStatus(200);
+        }).catch((err)=> {
+            console.log(`Server error while attempting ot add property: ${err}`);
+            res.sendStatus(400);
+        })
 
     }
 }
